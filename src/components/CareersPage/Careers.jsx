eaf382e3834bcf49e416649c497528c970c45c26
codingANDdecodin <130179661+codingANDdecodin@users.noreply.github.com>
2023-08-24 18:50:45 +0530

    Update Careers.jsx

--- src/components/CareersPage/Careers.jsx
+++ src/components/CareersPage/Careers.jsx
@@ -3,7 +3,7 @@ import Button from '../../Utilities/Button'
 import {TbTriangleInverted} from 'react-icons/tb'
 import {TbTriangle} from 'react-icons/tb'
 import Input from '../../Utilities/Input'
-//  import careerbg from  '../../img/career/careerbg.jpeg'
+// import careerbg from '../../img/career/careerbg.jpeg'
 function Careers() {
     const [findPosition,setFindPosition]=useState('');
     const [findLocation,setFindLocation]=useState('');
@@ -69,7 +69,7 @@ function Careers() {
         {
             position:"BACKEND DEVELOPER",
             role:"Backend Developer with emmbded c",
-            location:"Pune",
+            location:"mumbai",
             experience:"4 years",
             description:{
                 bullets: [
@@ -87,6 +87,9 @@ function Careers() {
         },
     ])
 
+    const [filterOpenings,setFilterOpenings]=useState([...openings])
+
+
 
     const OpenPosition=(props)=>{
         let [arrow,setArrow]=useState(false)
@@ -95,9 +98,9 @@ function Careers() {
         }
         return(
             <>
-              <div className='w-full text-left pl-12 py-8'>
-                  <div className='text-4xl font-semibold pb-4 border-b-2 border-gray-300 '>{props.position}</div>
-                  <div className='text-3xl flex  justify-between py-4 pr-4 font-thin' onClick={changeArrow}>
+              <div className='w-full text-left pl-12 py-8 md:pl-4'>
+                  <div className='text-4xl font-semibold pb-4 border-b-2 border-gray-300 md:text-2xl '>{props.position}</div>
+                  <div className='text-3xl flex  justify-between py-4 pr-4 font-thin md:text-xl' onClick={changeArrow}>
                        <p>{props.role}</p>
                        <div>
                         {
@@ -109,21 +112,21 @@ function Careers() {
                     </div>
                   <div>
                     {   arrow &&  <div className='mt-24'> 
-                            <p className='text-2xl font-semibold'>Location: <span className='font-normal'>{props.location}</span></p>
-                            <p className='text-2xl font-bold py-6'>Expreience: <span className='font-normal'>{props.experience}</span></p>
+                            <p className='text-2xl font-semibold md:text-xl'>Location: <span className='font-normal'>{props.location}</span></p>
+                            <p className='text-2xl font-bold py-6 md:text-xl' >Expreience: <span className='font-normal'>{props.experience}</span></p>
                             <div className='pl-6'>
                                 <ul>
                                     {
                                         props.description.bullets.map((i,index)=>{
                                             return(
-                                                <li key={index}   Style='list-style:circle' className='text-2xl py-4'>{i}</li>
+                                                <li key={index}   style={{'listStyle':'circle'}} className='text-2xl py-4 md:text-lg md:py-3 sm:pr-2'>{i}</li>
                                             )
                                         })
                                     }
                                 </ul>
                             </div>
-                            <div className='py-12'>
-                                <Button className={"text-3xl bg-yellow-400 text-white p-6"}>APPLY NOW</Button>
+                            <div className='py-12 md:flex md:justify-center'>
+                                <Button className={"text-3xl bg-yellow-400 text-white p-6 md:text-lg md:p-4 "}>APPLY NOW</Button>
                             </div>
                           </div>
                     
@@ -134,42 +137,43 @@ function Careers() {
         )
     }
 
-    const filterOpenings=()=>{
+    const filterOpeningsHandler=()=>{
         const uppercaseFindPosition = findPosition.toUpperCase();
+        const  uppercaseFindLocation=findLocation.toUpperCase();
         let filterData=openings.filter(data=> {
-            // let cheakPosition=data.position===findPosition ;
-            // console.log(data.position)
-            // console.log(findPosition)
-            //  return cheakPosition;
+
             const uppercasePosition = data.position.toUpperCase();
-            return uppercasePosition === uppercaseFindPosition;
-            })
-                console.log(filterData)
-        setOpenings(filterData)
+            const uppercaseLocation=data.location.toUpperCase();
+
+           
+             return uppercaseFindLocation===uppercaseLocation || uppercasePosition === uppercaseFindPosition;
+        })
+        setFilterOpenings(filterData)
     }
 
   return (
     <>
-       <div className="bg-yellow-300 flex  flex-col w-full py-24 text-7xl text-left px-16">
+       <div className="bg-yellow-300 flex  flex-col w-full py-24 text-7xl text-left px-16 md:text-4xl sm:text-3xl sm:px-6 ">
         <span>EXPLORE OUR</span>
         <span className='font-bold'>CURRENT OPENINGS</span>
 
    
             <form action=' '>
-            <div className='flex'>
+            <div className='flex md:flex-col  md:align-baseline'>
 
-                <div className='mt-10 w-1/5 '>
+                <div className='mt-10 w-1/5  md:w-2/5 sm:w-full'>
 
                     <Input type={'text'} className={' h-10 text-xl rounded-xl pl-2  '} placeholder={'enter position'} value={findPosition} onChange={(e)=>{setFindPosition(e.target.value)}}></Input>
                 </div>
 
-                <select className='h-10 text-xl px-10 mt-10 w-1/5 ml-8  rounded-xl ' value={findLocation} onChange={(e)=>{setFindLocation(e.target.value)}}>
+                <select className='h-10 text-xl px-10 mt-10 w-1/5 ml-8  rounded-xl md:w-2/5 md:ml-0 md:px-0 sm:w-full' value={findLocation} onChange={(e)=>{setFindLocation(e.target.value)}}>
+                    <option value=''>Location</option>
                     <option value="pune" >pune</option>
                     <option value="mumbai" >mumbai</option>
                 </select>
 
-                <div className='  w-1/5 ml-8 '>
-                    <Button type={'button'} onClick={filterOpenings} className={'text-lg border border-black w-full  rounded-xl h-10 bg-blue-500 '} >Job Search</Button>
+                <div className='  w-1/5 ml-8 md:w-2/5 md:ml-0 md:mt-10 sm:w-full'>
+                    <Button type={'button'} onClick={filterOpeningsHandler} className={'text-lg border border-black w-full  rounded-xl h-10 bg-blue-500 '} >Job Search</Button>
                 </div>
             </div>
 
@@ -178,9 +182,9 @@ function Careers() {
        </div>
          
        <div>
-        <p className='text-2xl text-left pl-12'>{openings.length} <span>jobs found</span></p>
+        <p className='text-2xl text-left pl-12 pt-4 md:text-lg md:pl-4'>{filterOpenings.length} <span>jobs found</span></p>
         {
-            openings.map((i,index)=>{
+            filterOpenings.map((i,index)=>{
                   return <OpenPosition key={index} position={i.position} role={i.role} location={i.location} experience={i.experience} description={i.description}></OpenPosition>
             })
         }
